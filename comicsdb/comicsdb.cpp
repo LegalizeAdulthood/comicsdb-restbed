0577e99d231d48855da5ee3a18a8bd8340ef5e2b
Richard <legalize@xmission.com>
2022-06-08 11:52:25 -0600

    GET comics resource

--- comicsdb/comicsdb.cpp
+++ comicsdb/comicsdb.cpp
@@ -1,9 +1,12 @@
 #include "comic.h"
 
+#include <restbed>
+
 #include <iostream>
+#include <string>
 #include <vector>
 
-int main()
+std::vector<comicsdb::Comic> load()
 {
   std::vector<comicsdb::Comic> db;
   db.emplace_back(comicsdb::fromJson(
@@ -19,6 +22,63 @@ int main()
     comic.colorist = "Stan Goldberg";
     db.push_back(comic);
   }
-  std::cout << comicsdb::toJson(db.back()) << '\n';
+  return db;
+}
+
+std::shared_ptr<restbed::Settings> getSettings()
+{
+    auto settings = std::make_shared<restbed::Settings>();
+    settings->set_default_header("Connection", "close");
+    return settings;
+}
+
+void publishResources(restbed::Service &service, std::vector<comicsdb::Comic> &db)
+{
+    auto comicResource = std::make_shared<restbed::Resource>();
+    comicResource->set_path("/comic/{id: [[:digit:]]+}");
+    comicResource->set_method_handler(
+        "GET",
+        [&db](const std::shared_ptr<restbed::Session> &session)
+        {
+          const auto &request = session->get_request();
+          const std::size_t id = request->get_path_parameter("id", 0);
+          if (id < db.size())
+          {
+            const std::string json = toJson(db[id]);
+            session->close(restbed::OK, json,
+                           {{"Content-Length", std::to_string(json.size())}});
+          }
+          else
+          {
+            const std::string msg{"Not Acceptable, id out of range"};
+            session->close(restbed::NOT_ACCEPTABLE, msg,
+                           {{"Content-Length", std::to_string(msg.size())},
+                            {"Content-Type", "text/plain"}});
+          }
+        });
+
+    service.publish(comicResource);
+}
+
+int main()
+{
+  try
+  {
+    std::vector<comicsdb::Comic> db = load();
+
+    restbed::Service service;
+    publishResources(service, db);
+    service.start(getSettings());
+  }
+  catch (const std::exception &bang)
+  {
+    std::cerr << bang.what() << '\n';
+    return 1;
+  }
+  catch (...)
+  {
+    return 1;
+  }
+
   return 0;
 }
